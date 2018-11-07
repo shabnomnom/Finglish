8c2fac9900f17a78b91c2f58c73799c6f1c2ffc1
mac <mac@macs-air.attlocal.net>
2018-11-06 22:51:59 -0800

    vocab data done

--- data_scrape.py
+++ data_scrape.py
@@ -5,31 +5,28 @@ import requests
 from bs4 import BeautifulSoup 
 import urllib, urllib.parse, urllib.error
 
-
+#get the resource 
 url='http://mylanguages.org/farsi_vocabulary.php'
 
+#open the page and read it 
 page = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(page, 'html.parser') 
 
+#get all the tables
 tables = soup.find_all('table')
 
-#print(tables)
+print(tables)
+
 # whatever this does covert my data to a csv 
-with open ('vocabs.csv', 'w') as csv:
-    for row in tables[0].find_all('tr'):
+#tr is row tag
+# th is column tag 
+with open ('vocabs_#.csv', 'w') as csv:
+    for row in tables[1].find_all('tr'):
         line = ""
         for td in row.find_all(['td','th']):
-            if "-" in td:
-                td.replace('-',",")
-                line += '"' + td.text + '",'
-                csv.write(line + '\n')
+            line += td.text + ','
+        #csv.write(line + '\n')
         
 
-# with open("vocabs.csv") as file:
-#     for row in file:
-#         if '-' in row:
-#             row.replace('-',",")
-#             print (row)
 
-
-
+# ran tables[0] and table [1] to have all the tables save the data in 
