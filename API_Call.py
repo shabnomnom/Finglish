9a0e53cc4dcca751e72c718c2a8fd82329642438
shabnomnom <shabnam.shomail@gmail.com>
2023-01-24 20:55:25 -0500

    initial app is working

--- API_Call.py
+++ API_Call.py
@@ -1,30 +1,19 @@
 # -*- coding: utf-8 -*- 
-
-
-
 from sys import argv
 from pprint import pprint 
 import json 
 import requests
 import os 
-
-
 forvo_key = os.environ["forvo_key"]  # os saves the token from the secrets.sh to a dictionary
                                                    # called os.environ.
                                                    # Run the source secrets.sh in the terminal before using.
-
-
 def word_url(farsi):
-
     key = forvo_key
     url="https://apifree.forvo.com/key/" + key + "/format/json/action/word-pronunciations/word/" + farsi + "/language/fa"
-
     # https://apifree.forvo.com/key/95a12f9924eaad8e79c2a57a985fe650/format/xml/action/word-pronunciations/word/cat/language/en
 
     req = requests.get(url)
-
     # print(req.url)
-
     # saving the request as a json file 
     # request already return a python dictionary 
     json_dict = req.json()
@@ -32,13 +21,8 @@ def word_url(farsi):
     if json_dict == {}:
         return None
     else:
-
-
         item_dict = json_dict["items"]
         # print(item_dict)
-
-
-
         words_url = []
         for item in item_dict:
             print("item:", item["pathmp3"] ) 
